bb0f755676895d55e1101abcba9cf6af06cca05a
arhik <karthik.katipalli@gmail.com>
2016-04-06 01:54:24 -0400

    included new style.css for custom styling

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 var Promise = require('bluebird'); // Promise polyfill for IE11
 import {bootstrap} from 'aurelia-bootstrapper-webpack';
 
+import '../styles/styles.css'
 import '../node_modules/semantic-ui/dist/semantic.min.css';
 import '../node_modules/semantic-ui/dist/semantic.min.js';
 
