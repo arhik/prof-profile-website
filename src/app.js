d63161bdadc7f45de6af5f001e8eedd262951623
arhik <karthik.katipalli@gmail.com>
2016-04-06 01:53:30 -0400

    new navigation menu routers in app.js

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Lance Fiondella';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'research',         name: 'research',        moduleId: 'research',        nav: true, title: 'Research' },
-      { route: 'students',  name: 'students', moduleId: 'students', nav: true, title: 'Students' }
+      { route: ['','research'], name: 'research',  moduleId: 'research',  nav: true, title: 'Research' },
+      { route: 'CV',   name: 'cv',  moduleId: 'cv',  nav:true, title: 'CV' },
+      { route: 'Publications',  name: 'publications', moduleId: 'publications', nav:true,title: 'Publications' },
+      { route: 'Students',  name: 'students', moduleId: 'students', nav:true,title: 'Students' },
+      { route: 'Projects',  name: 'projects', moduleId: 'projects', nav:true,title: 'Projects' }
     ]);
 
     this.router = router;
