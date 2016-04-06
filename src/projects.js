32aedfcad682646040fd8948b1c484a653bdc6f7
arhik <karthik.katipalli@gmail.com>
2016-04-06 02:01:59 -0400

    initial projects layout

---
+++ src/projects.js
@@ -0,0 +1,36 @@
+export class Projects{
+	constructor(){
+		this.present_projects = [
+		{
+			name:"Rotorcraft tradespace exploration",
+			fundedBy:"ARL",
+			summary:" Regarding Rotorcraft project |TODO|",
+			participants:["first","second"],
+			related_publications:["pub1","pub2","pub3"]
+		},
+		{
+			name:"Software Reliability Tool",
+			fundedBy:["NAVAIR","NSF"],
+			summary:" Regarding SRT |TODO|",
+			participants:["first","second"],
+			related_publications:["pub1","pub2","pub3"]
+
+		},
+		{
+			name:"Transportation",
+			fundedBy:["DHS"],
+			summary:" Regarding CREATE |TODO|",
+			participants:["first","second"],
+			related_publications:["pub1","pub2","pub3"]
+		},
+		{
+			name:"Theorem Proving",
+			fundedBy:[],
+			summary:" Regarding CREATE |TODO|",
+			participants:["first","second"],
+			related_publications:["pub1","pub2","pub3"]
+		}
+
+	]
+	}
+}
