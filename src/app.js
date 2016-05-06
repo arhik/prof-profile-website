29a6b8700e0a3bd5bf43892883af89f5d4125c68
arhik <karthik.katipalli@gmail.com>
2016-05-06 18:02:07 -0400

    changes in navigation bar

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Lance Fiondella';
     config.map([
-      { route: ['','research'], name: 'research',  moduleId: 'research',  nav: true, title: 'Research' },
-      { route: 'CV',   name: 'cv',  moduleId: 'cv',  nav:true, title: 'CV' },
-      { route: 'Publications',  name: 'publications', moduleId: 'publications', nav:true,title: 'Publications' },
+      { route: ['','CV'],   name: 'cv',  moduleId: 'cv',  nav:true, title: 'CV' },
+      // { route: ['','research'], name: 'research',  moduleId: 'research',  nav: true, title: 'Research' },
+      { route: 'teaching', name:'teaching', moduleId:'teaching', nav: true, title: "Teaching"},
+      // { route: 'Publications',  name: 'publications', moduleId: 'publications', nav:true,title: 'Publications' },
       { route: 'Students',  name: 'students', moduleId: 'students', nav:true,title: 'Students' },
       { route: 'Projects',  name: 'projects', moduleId: 'projects', nav:true,title: 'Projects' }
     ]);
