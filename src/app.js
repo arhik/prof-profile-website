1f19c54d9c2b8ceaea0feff545a50a0739e675f1
arhik <karthik.katipalli@gmail.com>
2016-05-09 23:25:10 -0400

    changes in layout

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Lance Fiondella';
     config.map([
-      { route: ['','research'], name: 'research',  moduleId: 'research',  nav: true, title: 'Research' },
-      { route: 'CV',   name: 'cv',  moduleId: 'cv',  nav:true, title: 'CV' },
-      { route: 'Publications',  name: 'publications', moduleId: 'publications', nav:true,title: 'Publications' },
+      // { route: ['','research'], name: 'research',  moduleId: 'research',  nav: true, title: 'Research' },
+      { route: ['','CV'],   name: 'cv',  moduleId: 'cv',  nav:true, title: 'CV' },
+      // { route: 'Publications',  name: 'publications', moduleId: 'publications', nav:true,title: 'Publications' },
       { route: 'Students',  name: 'students', moduleId: 'students', nav:true,title: 'Students' },
       { route: 'Projects',  name: 'projects', moduleId: 'projects', nav:true,title: 'Projects' }
     ]);
--- src/publications.js
+++ src/publications.js
@@ -4,8 +4,9 @@
 // $.fn.dimmer = require('semantic-ui-dimmer')
 // $.fn.transition = require("semantic-ui-transition")
 import {Bibtexparser} from "./bibtexparser"
-import {inject, bindable} from "aurelia-framework";
+import {inject, bindable, customElement} from "aurelia-framework";
 
+@customElement('publications')
 @inject(Bibtexparser)
 export class Publications{
 	// @bindable bibmodal;
@@ -18,7 +19,7 @@ export class Publications{
 	activate(){
 		this.pubs  = this.parser.getbib();
 		this.pubkeys = Object.keys(this.pubs);
-		// console.log(this.pubkeys)
+		console.log(this.pubkeys)
 		for(var i in this.pubs){
 			// console.log(this.pubs[i])
 			this.pubs[i].key = i;
@@ -28,6 +29,15 @@ export class Publications{
 	}
 
 	attached(){
+		this.pubs  = this.parser.getbib();
+		this.pubkeys = Object.keys(this.pubs);
+		console.log(this.pubkeys)
+		for(var i in this.pubs){
+			// console.log(this.pubs[i])
+			this.pubs[i].key = i;
+			this.publicationslist.push(this.pubs[i])
+		}
+		return this.publicationslist
 		// return $('.ui.modal').modal('show');
 	}
 
